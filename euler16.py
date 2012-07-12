66568977ad3ad2c6376507f9cd2a14a8fe79cc8d
Alex Bainton <rax-24@hotmail.com>
2012-07-12 10:08:19 +1000

    Added completed/incomplete challenge files

---
+++ euler16.py
@@ -0,0 +1,5 @@
+list = list(str(2**1000))
+for i in list:
+    x += int(i)
+
+print x
---
+++ euler17.py
@@ -0,0 +1,25 @@
+words_ones = {\
+        1: 'one',\
+        2: 'two',\
+        3: 'three',\
+        4: 'four',\
+        5: 'five',\
+        6: 'six',\
+        7: 'seven',\
+        8: 'eight',\
+        9: 'nine',\
+        10: 'ten',\
+        11: 'eleven',\
+        12: 'twelve',\
+        13: 'thirteen'}
+
+words_tens = {\
+        1: 'ten',\
+        2: 'twenty',\
+        3: 'thirty',\
+        4: 'fourty',\
+        5: 'fifty',\
+        6: 'sixty',\
+        7: 'seventy',\
+        8: 'eighty',\
+        9: 'ninety'}

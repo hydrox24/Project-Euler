08ad5e47bb976627916d94e7f203dacac94401bb
Alex Bainton <rax-24@hotmail.com>
2012-07-12 12:13:30 +1000

    Added start of euler17 logic

--- euler17.py
+++ euler17.py
@@ -1,3 +1,4 @@
+import re
 words_ones = {\
         1: 'one',\
         2: 'two',\
@@ -8,7 +9,6 @@ words_ones = {\
         7: 'seven',\
         8: 'eight',\
         9: 'nine',\
-        10: 'ten',\
         11: 'eleven',\
         12: 'twelve',\
         13: 'thirteen'}
@@ -23,3 +23,13 @@ words_tens = {\
         7: 'seventy',\
         8: 'eighty',\
         9: 'ninety'}
+
+for num in range(1,1000+1):
+    num_list = " ".join(str(num)).split(" ")
+    try:
+        print words_ones[int(num_list[-1])]
+    except KeyError:
+        if num_list[-1] == 1 or num_list[-1] == 2 num_list[-1] == 3
+            print words_ones[10]
+        elif num_
+        #print words_ones[int(num_list[-2]+"0")]
